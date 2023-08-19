cf5e02ec165eae8762e0590c250ec450fed04786
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-08-19 19:44:44 +0200

    update link front side

--- www/controllers/User.php
+++ www/controllers/User.php
@@ -5,7 +5,7 @@ namespace App\controllers;
 use App\core\User as Us;
 use App\core\View;
 use App\models\User as UserModel;
-use App\core\Verificator;
+use App\core\verificator\Verificator;
 use App\models\PasswordReset;
 use App\core\Session;
 use App\core\Sql;
@@ -15,7 +15,7 @@ class User
 {
     public function login()
     {
-        $view = new View("Login");
+        $view = new View("Login", "empty");
         $user = new UserModel();
         $view->assign("user", $user);
         
@@ -53,7 +53,7 @@ class User
     {
         $user = new UserModel();
 
-        $view = new View("Register");
+        $view = new View("Register", "empty");
         $view->assign("user", $user);
 
         if(empty($_POST)){
---
+++ www/views/home.php
@@ -0,0 +1,18 @@
+<main>
+    <section id="section1">
+        <div class="container">
+            <div>
+                <h1 class="title-animation">FoodPress<span class="head-title-cms">CMS</span></h1>
+                <h2>Une nouvelle façon de concevoir ses recettes</h2>
+                <a href="/register" id="start" class="button">Commencez gratuitement</a>
+            </div>
+        </div>
+    </section>
+    <section id="section2">
+        <div class="slider">
+            <img src="../public/assets/images/outils.png" alt="Erreur lors du chargement de l'image">
+            <img src="../public/assets/images/abonnement.png" alt="Erreur lors du chargement de l'image">
+            <img src="../public/assets/images/dashboard.png" alt="Erreur lors du chargement de l'image">
+        </div>
+    </section>
+</main>
