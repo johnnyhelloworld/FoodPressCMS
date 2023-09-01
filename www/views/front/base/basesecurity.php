a6b3e5714cb56a830eff98264b91eb5e98aca90f
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-09-01 19:50:54 +0200

    first part installeur

---
+++ www/views/front/base/basesecurity.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php include_once './views/front/partials/head.php' ?>
+
+<body>
+    <main style="padding:100px 50px">
+        <?= $base ?>
+    </main>
+    <?php include_once './views/front/partials/footer.php' ?>
+</body>
+
+</html>
