5599893e3900e3ffd237ebb48c32bab4ff17e06a
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-09-03 21:45:14 +0200

    fix redirection and fix views for recipe on client side

--- www/views/front/recipe/recipes.php
+++ www/views/front/recipe/recipes.php
@@ -1,17 +1,57 @@
-
 <?php ob_start(); ?>
 
-
-<h1>Recipes</h1>
+<div style="display:flex; align-items:center">
+    <h1>Nos recettes(<?= count($allRecipe) ?>)</h1>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <a style="text-decoration:none;border-radius:50%; background:grey;padding:5px 10px;color:white;" href="/createrecipe">+</a>
+</div>
 
 <?php if (isset($allRecipe)) : ?>
 
-    <?php foreach ($allRecipe as $recipe => $value) : ?>
-        <h1><?= ucfirst($value['title']) ?></h1>
-        <p><?= substr($value['content'], 0, 40) . '...' ?></p>
-        <hr>
-    <?php endforeach ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>Titre</th>
+                <th>Contenu</th>
+                <th>Visualiser en ligne</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($allRecipe as $recipe => $value) : ?>
+                <tr>
+                    <td><?= ucfirst($value['title']) ?></td>
+                    <td><?= substr($value['content'], 0, 50) . '...' ?></td>
+                    <td>
+                        <a href="/detailsRecipe?slug=<?= $value['slug'] ?>">
+                            <img src="../../../public/assets/images/eye.svg" alt="" width="20" height="20">
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach ?>
+        </tbody>
+    </table>
 <?php endif; ?>
+
+
+<style>
+    th,
+    td {
+        padding: 15px;
+        border: 1px solid grey;
+        margin: 0;
+        text-align: center;
+    }
+
+    table {
+        width: 100%;
+    }
+
+    thead,
+    tbody,
+    table {
+        border-collapse: collapse;
+    }
+</style>
 
 <?php $base = ob_get_clean(); ?>
 <?php require(__DIR__ . '../../base/base.php'); ?>
