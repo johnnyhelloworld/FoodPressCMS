bd803d8daa7d5701f34dac15d5dbd2c906fb4376
thomasR92 <thomas.riehl92@gmail.com>
2023-09-03 21:49:32 +0200

    change some colors

--- www/views/front/partials/footer.php
+++ www/views/front/partials/footer.php
@@ -4,7 +4,7 @@
             <li><a href="#">Termes et condition</a></li>
             <li><a href="#">Politique de confidentialité</a></li>
             <li><a href="https://github.com/johnnyhelloworld/FoodPressCMS">Documentation</a></li>
-            <li><a href="/">FoodPress<span class="bottom-title-cms">CMS</span></a></li>
+            <li><a href="/"><span class="bottom-title-cms">Food</span>PressCMS</a></li>
         </ul>
      </div>
  </footer>
