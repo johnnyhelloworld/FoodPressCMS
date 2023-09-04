31822b39075f9a8d46ebb1998b9dedb356fd2473
johnnyhelloworld <johnny58723826@hotmail.fr>
2023-09-04 22:13:13 +0200

    add conf.inc.php

---
+++ www/conf.inc.php
@@ -0,0 +1,14 @@
+
+<?php
+define("DB_NAME", "dck64f7g7cu6nj");
+define("DB_DRIVER", "pgsql");
+define("DB_HOST", "ec2-52-215-68-14.eu-west-1.compute.amazonaws.com");
+define("DB_PORT", "5432");
+define("DB_USER", "jqketjftmnjaip");
+define("DB_PWD", "b16157d85eb9218c3bf3b90d8025078c54b67b9ea33f489f7109fd65c1685060");
+define("DB_PREFIX", "fp_");
+
+define("MAILHOST", "smtp.gmail.com");
+define("MAILPORT", 465);
+define("MAILUSER", "lokoshi21@gmail.com");
+define("MAILPSWD", "mfpzrurmqggjsveg");
